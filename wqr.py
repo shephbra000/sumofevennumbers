efcd2efa42e615b79b4bb15c88d219a8ae6fe042
shephbra000 <shephbra000@warren.k12.in.us>
2024-11-22 07:39:52 -0500

    Initial commit

---
+++ wqr.py
@@ -0,0 +1,7 @@
+# TODO: Ask the user for how many rows.  and store in a variable called rows
+
+for row in range(1, rows + 1):
+    for star in range(1, row + 1):
+        # TODO: print a * and a space, print function should have this at the end endl=""
+        # Example: print("stuff", end="")
+    print()
